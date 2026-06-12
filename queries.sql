ffd39bd9b881b3b4022e0b3f0f30042362a381a0
Kiranjot Kaur <kiranjotkaurkalra2004@gmail.com>
2026-06-12 17:31:43 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -28,3 +28,122 @@ WHERE
     sale_date IS NULL OR sale_time IS NULL OR customer_id IS NULL OR
     gender IS NULL OR age IS NULL OR category IS NULL OR
     quantity IS NULL OR price_per_unit IS NULL OR cogs IS NULL;
+
+
+
+# Retail Sales Analysis - SQL Business Questions
+-- Retail Sales Analysis Project
+-- SQL Business Questions & Solutions
+
+-- 1. Retrieve all sales made on 5-Nov-2022
+
+SELECT *
+FROM retail_sales
+WHERE sale_date = '2022-11-05';
+
+
+-- 2. Find Clothing transactions with quantity greater than 4 in Nov-2022
+
+SELECT *
+FROM retail_sales
+WHERE category = 'Clothing'
+  AND TO_CHAR(sale_date, 'YYYY-MM') = '2022-11'
+  AND quantity >= 4;
+
+
+-- 3. Calculate total sales and total orders for each category
+
+SELECT
+    category,
+    SUM(total_sale) AS net_sale,
+    COUNT(*) AS total_orders
+FROM retail_sales
+GROUP BY category;
+
+
+-- 4. Find the average age of customers who purchased Beauty products
+
+SELECT
+    ROUND(AVG(age), 2) AS avg_age
+FROM retail_sales
+WHERE category = 'Beauty';
+
+
+-- 5. Retrieve transactions where total sale amount is greater than 1000
+
+SELECT *
+FROM retail_sales
+WHERE total_sale > 1000;
+
+
+-- 6. Count transactions by gender and category
+
+SELECT
+    category,
+    gender,
+    COUNT(*) AS total_transactions
+FROM retail_sales
+GROUP BY category, gender
+ORDER BY category;
+
+
+-- 7. Find the best-selling month in each year based on average sales
+
+SELECT
+    year,
+    month,
+    avg_sale
+FROM (
+    SELECT
+        EXTRACT(YEAR FROM sale_date) AS year,
+        EXTRACT(MONTH FROM sale_date) AS month,
+        AVG(total_sale) AS avg_sale,
+        RANK() OVER (
+            PARTITION BY EXTRACT(YEAR FROM sale_date)
+            ORDER BY AVG(total_sale) DESC
+        ) AS rank
+    FROM retail_sales
+    GROUP BY 1, 2
+) AS t1
+WHERE rank = 1;
+
+
+-- 8. Find the top 5 customers with the highest total sales
+
+SELECT
+    customer_id,
+    SUM(total_sale) AS total_sales
+FROM retail_sales
+GROUP BY customer_id
+ORDER BY total_sales DESC
+LIMIT 5;
+
+
+-- 9. Count unique customers for each product category
+
+SELECT
+    category,
+    COUNT(DISTINCT customer_id) AS unique_customers
+FROM retail_sales
+GROUP BY category;
+
+
+-- 10. Categorize sales into shifts and count total orders in each shift
+
+WITH hourly_sale AS (
+    SELECT *,
+           CASE
+               WHEN EXTRACT(HOUR FROM sale_time) < 12 THEN 'Morning'
+               WHEN EXTRACT(HOUR FROM sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+               ELSE 'Evening'
+           END AS shift
+    FROM retail_sales
+)
+
+SELECT
+    shift,
+    COUNT(*) AS total_orders
+FROM hourly_sale
+GROUP BY shift
+ORDER BY total_orders DESC;
+```
