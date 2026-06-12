6367f32d11e94fd16f70fb1fa580c67835db7581
Kiranjot Kaur <kiranjotkaurkalra2004@gmail.com>
2026-06-12 17:26:43 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -15,3 +15,16 @@ CREATE TABLE IF NOT EXISTS retail_sales
     cogs FLOAT,
     total_sale FLOAT
 );
+SELECT COUNT(*) FROM retail_sales;
+SELECT COUNT(DISTINCT customer_id) FROM retail_sales;
+SELECT DISTINCT category FROM retail_sales;
+SELECT * FROM retail_sales
+WHERE
+    sale_date IS NULL OR sale_time IS NULL OR customer_id IS NULL OR
+    gender IS NULL OR age IS NULL OR category IS NULL OR
+    quantity IS NULL OR price_per_unit IS NULL OR cogs IS NULL;
+DELETE FROM retail_sales
+WHERE
+    sale_date IS NULL OR sale_time IS NULL OR customer_id IS NULL OR
+    gender IS NULL OR age IS NULL OR category IS NULL OR
+    quantity IS NULL OR price_per_unit IS NULL OR cogs IS NULL;
