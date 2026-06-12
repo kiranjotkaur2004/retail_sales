2f02e2340aeee5ade43866e55e04f23a5852d35a
Kiranjot Kaur <kiranjotkaurkalra2004@gmail.com>
2026-06-12 17:23:20 +0530

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS retail_db;
+
+USE retail_db;
+
+CREATE TABLE IF NOT EXISTS retail_sales
+(
+    transactions_id INT PRIMARY KEY,
+    sale_date DATE,
+    sale_time TIME,
+    customer_id INT,
+    gender VARCHAR(10),
+    age INT,
+    category VARCHAR(35),
+    quantity INT,
+    price_per_unit FLOAT,
+    cogs FLOAT,
+    total_sale FLOAT
+);
